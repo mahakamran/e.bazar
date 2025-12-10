8840475fdaff1f4e54144d65af6074541cc0a4a8
alikamran244 <your.email@example.com>
2025-12-10 01:45:18 -0800

    n

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.use("/uploads", express.static("uploads"));
 
 
